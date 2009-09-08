31d9bd73b733c165a5e9f8a60cd13567b6ce082f
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-09-08 22:18:26 +1000

    added glade / gtk+ skeleton

--- src/desktop/main.py
+++ src/desktop/main.py
@@ -1,70 +1,62 @@
-import wx
-import wx.html
-
+#!/opt/local/bin/python2.5
+# to run (in vim)
+# !`pwd`/%
 import sys
+import os
+import urllib
 
-from feeditem import FeedItem
+import gtk
+import gobject
+import webkit
 
-sys.path.append('..')
-from sync import main as sync_main
-from sync.main import app_globals
-
-global db
-
-class MainFrame(wx.Frame):
+class Gris(object):
 	def __init__(self):
-		wx.Frame.__init__(self, None, title='GRiS')
-		self.Bind(wx.EVT_CLOSE, self.on_close)
+		self.folders = ("images", "text", "foo")
+		self.init_ui()
 
-		panel = wx.Panel(self)
+	def on_window_destroy(self, widget, data=None):
+		gtk.main_quit()
 	
-		splitter = wx.SplitterWindow(self, -1, style = wx.SP_LIVE_UPDATE)
-		self.item_view = self.init_html(splitter)
-		self.feed_list = self.init_list(splitter)
-		
-		splitter.SetMinimumPaneSize(80)
-		splitter.SplitVertically(self.feed_list, self.item_view)
-		splitter.SetSashPosition(200)
+	def init_ui(self):
+		builder = gtk.Builder()
+		builder.add_from_file("main.glade")
 
-		sizer = wx.BoxSizer(wx.VERTICAL)
-		sizer.Add(splitter,1,wx.EXPAND)
+		def set_object(name):
+			setattr(self, name, builder.get_object(name))
+			
+		map(set_object, ("window", "feed_tree_view", "content_scroll_view"))
+		builder.connect_signals(self)
+		self.init_feeds()
+		self.init_columns()
+		self.init_content()
 
-		# layout
-		self.SetSizer(sizer)
-		self.SetAutoLayout(1)
-		sizer.Fit(self)
+	def on_feed_tree_view_select_row(self, widget, data=None):
+		_dir = os.path.dirname(os.path.abspath(__file__))
+		base = 'file://' + urllib.quote(_dir)
+		selected = []
+		col = 0
+		store, iter = self.feed_tree_view.get_selection().get_selected()
+		feed_name = store.get_value(iter, col)
+		self.content_view.load_html_string("<h1>yay!</h1><pre>loaded: %r</pre>" % (feed_name,), base)
 
-		self.Show(True)
+	def init_content(self):
+		self.content_view = webkit.WebView()
+		self.content_scroll_view.add(self.content_view)
+		self.content_scroll_view.show_all()
 
-	def on_close(self, event):
-		print "closed!"
-		self.Destroy()
+	def init_feeds(self):
+		store = self.feed_tree_store = gtk.TreeStore(gobject.TYPE_STRING, gobject.TYPE_INT)
+		store.set(store.append(None), 0, "all feeds", 1, 500)
+		store.set(store.append(None), 0, "more feeds", 1, 200)
+		self.feed_tree_view.set_model(store)
 	
-	def init_html(self, parent):
-		wx.InitAllImageHandlers()
-		item_view = wx.html.HtmlWindow(parent, -1)
-		item_view.SetPage("<b>HI!</b>")
-		return item_view
-	
-	def init_list(self, parent):
-		feed_list = wx.TreeCtrl(parent, -1)
-		# feed_list = wx.Window(parent, style=wx.BORDER_SUNKEN)
-		# wx.TreeCtrl(feed_list, -1)
-		return feed_list
+	def init_columns(self):
+		view = self.feed_tree_view
+		view.append_column(gtk.TreeViewColumn('title', gtk.CellRendererText(), text=0))
+		view.append_column(gtk.TreeViewColumn('items', gtk.CellRendererText(), text=1))
 
-def run_wx():
-	app = wx.PySimpleApp()
-	frame = MainFrame()
-	app.MainLoop()
-	print "exiting..."
-	sync_main.cleanup()
+if __name__ == "__main__":
+	gris = Gris()
+	gris.window.show()
+	gtk.main()
 
-def main():
-	global db
-	sync_main.setup(['--output-path=~/.GRiS'])
-	db = app_globals.DATABASE = sync_main.DB()
-	items = db.get_items()
-	run_wx()
-
-if __name__ == '__main__':
-	main()
